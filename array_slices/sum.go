bf8e89e10be42c57ba4ca043cad263706969fc8a
Fernando Henrique <liquuid@gmail.com>
2019-02-20 11:38:59 -0300

    finishing sum tdd

--- array_slices/sum.go
+++ array_slices/sum.go
@@ -7,3 +7,24 @@ func Sum(arr []int) int {
 	}
 	return sum
 }
+
+func SumAll(arr ...[]int) []int{
+	var sums []int
+	for _, num := range arr{
+		sums = append(sums, Sum(num))
+	}
+	return sums
+}
+
+func SumAllTails(arr ...[]int) []int{
+	var sums []int
+	for _, nums := range arr{
+		if len(nums) != 0 {
+			tail := nums[1:]
+			sums = append(sums, Sum(tail))
+		} else {
+			sums = append(sums, 0)
+		}
+	}
+	return sums
+}
--- array_slices/sum_test.go
+++ array_slices/sum_test.go
@@ -2,6 +2,7 @@ package array_slices
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -25,4 +26,31 @@ func TestSum(t *testing.T) {
 			}
 		})
 	}
+}
+
+
+func TestSumAllTails(t *testing.T){
+	checkSums := func(t *testing.T, got, want []int){
+		t.Helper()
+		if !reflect.DeepEqual(got, want){
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		result := SumAllTails([]int{1, 2, 3}, []int{2, 9, 10, 1}, []int{5, 5}, []int{4})
+		expected := []int{5, 20, 5, 0}
+
+		checkSums(t, result, expected)
+
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		result := SumAllTails([]int{}, []int{2, 9, 10, 1})
+		expected := []int{0, 20}
+
+		checkSums(t, result, expected)
+	})
+
+
 }
